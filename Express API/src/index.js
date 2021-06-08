aa6b5b4b62335e2800e766ca421e605af3aceea3
piacz <piaczernyk@gmail.com>
2021-06-08 15:53:52 -0300

    updating files

--- Express API/src/index.js
+++ Express API/src/index.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { clientOrigins, serverPort, domain, apiId, apiSecret } = require("./config/env.dev");
 
-const { messagesRouter } = require("./messages/messages.router");
 const { listsRouter } = require("./lists/lists.router");
 
 /**
@@ -27,7 +26,6 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
-apiRouter.use("/messages", messagesRouter);
 apiRouter.use("/lists", listsRouter);
 
 app.use(function (err, req, res, next) {
