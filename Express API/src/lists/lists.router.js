225a1d1e0618bd368f78f75915d20fb9bdff04c3
piacz <piaczernyk@gmail.com>
2021-06-09 16:39:41 -0300

    CheckJwt

--- Express API/src/lists/lists.router.js
+++ Express API/src/lists/lists.router.js
@@ -21,9 +21,8 @@ var tokenRequest = {
   };
 
 // GET lists/
-// checkJwt
 
-listsRouter.get("/rules-lists", async (req, res) => {
+listsRouter.get("/rules-lists", checkJwt, async (req, res) => {
     try {
         let managementToken = (await axios.request(tokenRequest)).data.access_token;
 
