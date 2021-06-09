bddef01af162d0e99c3cf5715ffd83b243f163a9
piacz <piaczernyk@gmail.com>
2021-06-09 11:02:59 -0300

    Home updates

--- React SPA/src/views/home.js
+++ React SPA/src/views/home.js
@@ -10,9 +10,9 @@ const Home = () => (
       <div className='homeText'>
         <p>This is a sample application that 
           demonstrates a connection with <a href='https://auth0.com/docs/api/management/v2'>Auth0's Management 
-          API</a> to access dynamically every application configured on a 
-          tenant and their respective applied rules on the <a href='http://localhost:4040/external-api'>Apps List section</a>. The endpoints 
-          used through an Express API are:
+          API</a> to dynamically access every application configured on a 
+          tenant and their respective rules applied, showing them on the <a href='http://localhost:4040/external-api'>Apps List section</a>. The endpoints 
+          called through an Express API are:
         </p>
         <ul>
           <li>/api/v2/rules</li>
